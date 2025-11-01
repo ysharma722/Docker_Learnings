c79e35f1cd3dbbd26faad2bbd78a9342d77a76b2
ysharma722 <ysharma722@gmail.com>
2025-11-01 09:32:57 +0000

    App.js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! kya change instant hoga ?
+          Hello World! Trying out to trigger workflow
         </p>
         <a
           className="App-link"
